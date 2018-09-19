748939b76cf5b7dd7ec8a17fd858d209d26b75f8
Raja David Hasugian <rajadavidh@yahoo.com>
2018-09-19 21:25:22 +0700

    add Data Normalization summary

--- 2-joins.sql
+++ 2-joins.sql
@@ -1,1 +1,19 @@
--- TODO ------------------------------
+/*
+Database Normalization
+When creating a database, it is really important to think about how data will be stored.
+This is known as normalization, and it is a huge part of most SQL classes.
+If you are in charge of setting up a new database, it is important to have a thorough understanding of database normalization.
+
+There are essentially three ideas that are aimed at database normalization:
+
+1. Are the tables storing logical groupings of the data?
+2. Can I make changes in a single location, rather than in many tables for the same information?
+3. Can I access and manipulate data quickly and efficiently?
+
+This is discussed in detail here: https://www.itprotoday.com/microsoft-sql-server/sql-design-why-you-need-database-normalization
+
+However, most analysts are working with a database that was already set up with the necessary properties in place.
+As analysts of data, you don't really need to think too much about data normalization.
+You just need to be able to pull the data from the database, so you can start drawing insights.
+This will be our focus in this lesson.
+*/
