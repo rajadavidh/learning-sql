4cc3e8c72e324717e818d45d10b1b2bdf03fd3d2
Raja David Hasugian <rajadavidh@yahoo.com>
2018-09-24 15:22:49 +0700

    add section: JOIN quiz part I

--- 2-joins.sql
+++ 2-joins.sql
@@ -214,3 +214,49 @@ JOIN tablename2 AS t2
 | example row | example row |
 | example row | example row |
 */
+
+-- Quiz: JOIN part I
+
+/*
+1. Provide a table for all web_events associated with account name of Walmart. There should be three columns.
+Be sure to include the primary_poc, time of the event, and the channel for each event.
+Additionally, you might choose to add a fourth column to assure only Walmart events were chosen.
+*/
+
+SELECT a.name, a.primary_poc, w.occurred_at, w.channel
+FROM accounts AS a
+JOIN web_events AS w
+ON a.id = w.account_id
+WHERE a.name = 'Walmart';
+
+
+/*
+2. Provide a table that provides the region for each sales_rep along with their associated accounts.
+Your final table should include three columns: the region name, the sales rep name, and the account name.
+Sort the accounts alphabetically (A-Z) according to account name.
+*/
+
+SELECT r.name region_name, s.name sales_rep_name, a.name account_name
+FROM region AS r
+JOIN sales_reps AS s
+ON r.id = s.region_id
+JOIN accounts AS a
+ON a.sales_rep_id = s.id
+ORDER BY a.name ASC;
+
+/*
+3. Provide the name for each region for every order,
+as well as the account name and the unit price they paid (total_amt_usd/total) for the order.
+Your final table should have 3 columns: region name, account name, and unit price.
+A few accounts have 0 for total, so I divided by (total + 0.01) to assure not dividing by zero.
+*/
+
+SELECT r.name region_name, a.name account_name, o.total_amt_usd / (o.total+0.01) unit_price
+FROM region as r
+JOIN sales_reps AS s
+ON r.id = s.region_id
+JOIN accounts AS a
+ON a.sales_rep_id = s.id
+JOIN orders AS o
+ON o.account_id = a.id
+LIMIT 10;
