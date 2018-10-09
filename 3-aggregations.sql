da2b312080ce3fa9ab98f6c22e5f444b4825deb8
Raja David Hasugian <rajadavidh@yahoo.com>
2018-10-10 01:12:41 +0700

    add section: SUM quiz questions

--- 3-aggregations.sql
+++ 3-aggregations.sql
@@ -76,3 +76,30 @@ An important thing to remember: aggregators only aggregate vertically - the valu
 If you want to perform a calculation across rows, you would do this with simple arithmetic:
 https://community.modeanalytics.com/sql/tutorial/sql-operators/#arithmetic-in-sql
 */
+
+-- Quiz: SUM
+
+/*
+1. Find the total amount of poster_qty paper ordered in the orders table.
+*/
+
+
+/*
+2. Find the total amount of standard_qty paper ordered in the orders table.
+*/
+
+/*
+3. Find the total dollar amount of sales using the total_amt_usd in the orders table.
+*/
+
+
+/*
+4. Find the total amount spent on standard_amt_usd and gloss_amt_usd paper for each order in the orders table.
+This should give a dollar amount for each order in the table.
+*/
+
+
+/*
+5. Find the standard_amt_usd per unit of standard_qty paper.
+Your solution should use both an aggregation and a mathematical operator.
+*/
