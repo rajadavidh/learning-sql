c31bf143b9cb568baeeb65a08269595cde0a8db5
Raja David Hasugian <rajadavidh@yahoo.com>
2018-10-10 01:27:17 +0700

    add section: SUM quiz answers for #1 - #3

--- 3-aggregations.sql
+++ 3-aggregations.sql
@@ -83,23 +83,33 @@ https://community.modeanalytics.com/sql/tutorial/sql-operators/#arithmetic-in-sq
 1. Find the total amount of poster_qty paper ordered in the orders table.
 */
 
+SELECT SUM(poster_qty) AS poster
+FROM orders;
 
 /*
 2. Find the total amount of standard_qty paper ordered in the orders table.
 */
 
+SELECT SUM(standard_qty) AS standard
+FROM orders;
+
 /*
 3. Find the total dollar amount of sales using the total_amt_usd in the orders table.
 */
 
+SELECT SUM(total_amt_usd) AS total_sales
+FROM orders;
 
 /*
 4. Find the total amount spent on standard_amt_usd and gloss_amt_usd paper for each order in the orders table.
 This should give a dollar amount for each order in the table.
 */
 
+-- TODO
 
 /*
 5. Find the standard_amt_usd per unit of standard_qty paper.
 Your solution should use both an aggregation and a mathematical operator.
 */
+
+-- TODO
