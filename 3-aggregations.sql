a223f5e0d293da15a7cc577b07c77423e091e847
Raja David Hasugian <rajadavidh@yahoo.com>
2018-10-10 11:43:46 +0700

    add section: AVG explanation and example + MEDIAN Tips

--- 3-aggregations.sql
+++ 3-aggregations.sql
@@ -166,3 +166,27 @@ SELECT MIN(standard_qty) AS standard_min,
        MAX(gloss_qty) AS gloss_max,
        MAX(poster_qty) AS poster_max,
 FROM orders;
+
+/*
+AVG
+Similar to other software AVG returns the mean of the data - that is
+the sum of all of the values in the column divided by the number of values in a column.
+This aggregate function again ignores the NULL values in both the numerator and the denominator.
+
+If you want to count NULLs as zero, you will need to use SUM and COUNT.
+However, this is probably not a good idea if the NULL values truly just represent unknown values for a cell.
+*/
+
+-- Example:
+
+SELECT AVG(standard_qty) AS standard_avg,
+       AVG(gloss_qty) AS gloss_avg,
+       AVG(poster_qty) AS poster_avg
+FROM orders;
+
+/*
+MEDIAN - Expert Tip
+One quick note that a median might be a more appropriate measure of center for this data,
+but finding the median happens to be a pretty difficult thing to get using SQL alone —
+so difficult that finding a median is occasionally asked as an interview question.
+*/
